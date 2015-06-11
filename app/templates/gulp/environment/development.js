b7659af1cb1fa84f934c3ccd951c2c5badc9c124
Kevin Altman <kevin@itsthatguy.com>
2015-06-11 15:03:47 -0500

    removes unused environment dependencies

--- app/templates/gulp/environment/development.js
+++ app/templates/gulp/environment/development.js
@@ -1,10 +1,5 @@
-/* global config, path */
 'use strict';
 
 module.exports = {
-  vendor: [
-    path.join(config.PROJECT_ROOT, 'bower_components/formerjs/index.js'),
-    path.join(config.PROJECT_ROOT, 'bower_components/owl.carousel/dist/owl.carousel.js'),
-    path.join(config.PROJECT_ROOT, 'bower_components/owl.carousel/dist/assets/owl.carousel.css')
-  ]
+  vendor: []
 };
--- app/templates/gulp/environment/production.js
+++ app/templates/gulp/environment/production.js
@@ -1,9 +1,5 @@
-/* global config, path */
 'use strict';
 
 module.exports = {
-  vendor: [
-    path.join(config.PROJECT_ROOT, 'bower_components/owl.carousel/dist/owl.carousel.min.js'),
-    path.join(config.PROJECT_ROOT, 'bower_components/owl.carousel/dist/assets/owl.carousel.min.css')
-  ]
+  vendor: []
 };
